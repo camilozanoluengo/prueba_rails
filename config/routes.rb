b99447b825332cd80d656f78b7f7bd73a0c208d1
camilo <camilo.lozano@bci.com>
2018-01-24 21:25:41 -0300

    commit prueba_ruby

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+get '/todos', to:'todos#index'
+get 'todos/new', to:'todos#new'
+post '/todos', 'todos#create'
+get '/todos/list', 'todos#list'
+
+get '/todos/:id', to: 'todos#show', as: 'todo'
+get '/todos/:id/edit', to: 'todos#edit', as: 'todos_edit'
+patch '/todos/:id', to: 'todos#update'
+delete '/todos/:id', to: 'todos#destroy', as: 'todos_delete'
+get '/todos/:id/complete', to: 'todos#complete', as: 'todos_complete'
+
+root 'todos#index'
+
+
+end
